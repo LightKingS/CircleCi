26f5fb0fde341987c81da513d7913d5453ba8213
André Luz <andrereisdaluz@hotmail.com>
2021-05-06 11:56:19 -0300

    :tada: Adds the initial project files

---
+++ src/service/sellProduct.js
@@ -0,0 +1,14 @@
+/**
+ * A quantidade vendida pode ser de 1 ou mais unidades
+ * --> Se estoque ficar negativo uma exception deve ser lançada
+ * --> O valor de venda não pode ser maior que o valor de compra
+ * @param {*} product 
+ * @param {*} amount 
+ */
+
+import Product from '../model/product'
+
+export default function sellProduct(product, amount){
+    product.stock -= 1;
+    return product;
+}
